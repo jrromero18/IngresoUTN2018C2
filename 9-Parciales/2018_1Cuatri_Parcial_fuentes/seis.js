614d8cad4d55c82ee43f160fac7f265e8f592dab
jrromero18 <36246860+jrromero18@users.noreply.github.com>
2018-07-31 00:10:09 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,48 @@
 function mostrar()
 {
     
+var hora
+
+hora = parseInt(document.getElementById("laHora").value);
+
+switch (hora) 
+{
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:  
+    alert("es de mañana");  
+    break;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    alert("es de tarde");
+    break;
+     
+
+    default:
+    
+    if (hora>0 && hora <=5 || hora>=20 && hora <= 24)
+{
+    alert ("Es de noche");
+    alert("A dormir");
+    break;
+}
+
+
+      alert("la hora es incorrecta");
+
+    break;
+}
+
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
 
+
+var notas;
+var sexo;
+var cantidad=0;
+var acunotas=0;
+
+
+ notas = parseInt(prompt("Ingrese una nota"));
+
+while (notas<0 || notas>=10)
+{
+
+  notas = parseInotasnt(prompt("ERROR!! Ingrese una nota"));
+
+}
+
+sexo = (prompt("Ingrese un sexo"));
+
+while (!(sexo=="f" || sexo=="m"))
+{
+
+    sexo = (prompt("ERROR!! Ingrese una sexo valido"));
+
+}
+acunotas = notas + acunotas;
+cantidad ++
+
+
+alert("nota " + acunotas + "sexo " +sexo);
+
+
+
+
+
+
+//sexo = prompt("ingrese un sexo");
+
+
+
+
+
 }
